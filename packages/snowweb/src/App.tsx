00b9f6b2fc49f2908c3f266d27de0af0f377e438
jimmyxuexue <1002661758@qq.com>
2022-11-16 18:00:04 +0800

    ✨ useEventLinstener

--- packages/snowweb/src/App.tsx
+++ packages/snowweb/src/App.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import UseImageDemo from '@daily-store/snowhooks/src/useImage/demo'
+import UseEventListenerDOM from '@daily-store/snowhooks/src/useEventListener/demo'
 
 function App() {
 	const [count, setCount] = useState(0)
@@ -7,6 +8,7 @@ function App() {
 	return (
 		<div className="App">
 			<UseImageDemo />
+			<UseEventListenerDOM />
 		</div>
 	)
 }
