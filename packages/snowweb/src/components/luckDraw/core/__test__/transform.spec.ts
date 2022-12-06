fbab40fe1a558f95c3964753f48e24c681ec2ff6
jimmyxuexue <1002661758@qq.com>
2022-12-06 18:05:49 +0800

    ✅ snowweb add jest and some test case

---
+++ packages/snowweb/src/components/luckDraw/core/__test__/transform.spec.ts
@@ -0,0 +1,14 @@
+import { transformWinPrizeItem } from '../transform'
+
+describe('>>> transformWinPrizeItem', () => {
+	it('the Fourth item should mapped Eighth in layout', () => {
+		expect(transformWinPrizeItem(4)).toBe(8)
+	})
+
+	it('the Ninth item should be throw error', () => {
+		// expect arguments should transmit an function when should test throw error
+		expect(() => transformWinPrizeItem(9)).toThrow(
+			"key couldn't allow bigger than 8"
+		)
+	})
+})
