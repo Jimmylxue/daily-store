d1a808579a59217c8eb01f6912fdc97deaac7282
jimmyxuexue <1002661758@qq.com>
2023-01-11 20:16:12 +0800

    :zap: 实现assert

--- packages/note/JavaScript/proxy/assert.js
+++ packages/note/JavaScript/proxy/assert.js
@@ -3,7 +3,9 @@ const assert = new Proxy(
 	{
 		set(target, check, value) {
 			if (!value) {
-				console.log('error :', check)
+				console.log('fail :', check)
+			} else {
+				console.log('pass :', check)
 			}
 			return true
 		},
@@ -14,3 +16,10 @@ const assert = new Proxy(
 )
 
 assert['check 15 is smaller than 17'] = 15 > 17
+
+function checkCount(count) {
+	return count > 25
+}
+
+assert['function should return true when count is 30'] = checkCount(30)
+assert['function should return false when count is 22'] = checkCount(22)
