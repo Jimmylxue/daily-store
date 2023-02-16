0fa4babeab78315da0956f1eff7b9e4dab3770f4
jimmyxuexue <1002661758@qq.com>
2023-02-16 10:29:05 +0800

    🔧 更新cc文件

--- packages/nodejs/node-gyp/keyboard/keyboard.cc
+++ packages/nodejs/node-gyp/keyboard/keyboard.cc
@@ -1,31 +1,46 @@
 #include <napi.h> // 使用Napi的namespace还有最后的NODE_API_MODULE(addon,Init)都是模板化的，照抄过来不用动。
-
+#include "napi-thread-safe-callback.hpp"
+#include <iostream>
 #include <windows.h>
 
 using namespace Napi;
+using namespace std;
 
 HHOOK keyboardHook;
 
 std::shared_ptr<ThreadSafeCallback> cb;
+Napi::Env env = NULL;
 
 String Hello(const CallbackInfo &info)
 {
   return String::New(info.Env(), "world");
 }
 
-LRESULT CALLBACK KeyboardEvent(int nCode, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK KeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
   PKBDLLHOOKSTRUCT p = (PKBDLLHOOKSTRUCT)lParam; // 创建p变量
+
   if (nCode >= 0)
   {
     if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP)
     {
-      std::string key = std::to_string(p->vkCode); // 创建 key 是根据p做某种字符类型的转换
+      // std::string key = checkKeyTable(p->vkCode);
+      std::string key = std::to_string(p->vkCode);
       cb->call([key](Napi::Env e, std::vector<napi_value> &args)
-               { args = {Napi::String::New(e, key)}; }); // 大致意思调用全局的 cb 函数
+               { args = {Napi::String::New(e, key)}; });
     }
   }
-  return CallNextHookEx(keyboardHook, nCode, wParam, lParam);
+
+  // if (nCode >= 0)
+  // {
+  //   if (wParam == WM_KEYUP || wParam == WM_SYSKEYUP)
+  //   {
+  //     std::string key = std::to_string(p->vkCode); // 创建 key 是根据p做某种字符类型的转换
+  //     cb->call([key](Napi::Env e, std::vector<napi_value> &args)
+  //              { args = {Napi::String::New(e, key)}; }); // 大致意思调用全局的 cb 函数
+  //   }
+  // }
+  // return CallNextHookEx(keyboardHook, nCode, wParam, lParam);
 }
 
 void setKeyboardHook()
@@ -35,7 +50,7 @@ void setKeyboardHook()
    *  常用于监听 鼠标 或者 键盘的一些钩子设置
    *
    */
-  keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardEvent, NULL, 0);
+  keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardProc, NULL, 0);
   std::cout << "set hook\n";
   // Fail to set hook
   if (keyboardHook == NULL)
@@ -52,7 +67,7 @@ void start()
   setKeyboardHook();
 }
 
-void listen(const CallbackInfo &info)
+void listen(const Napi::CallbackInfo &info)
 {
   // 用 cb 接受 js调用listen时传的一个回调函数
   cb = std::make_shared<ThreadSafeCallback>(info[0].As<Napi::Function>());
