6e8139ae2dffd14e81afe243064f8fe6f63423be
jimmyxuexue <1002661758@qq.com>
2023-03-12 20:47:58 +0800

    ✨ skeleton 组件

--- packages/snowweb/src/components/snowDesign/Skeleton/index.tsx
+++ packages/snowweb/src/components/snowDesign/Skeleton/index.tsx
@@ -1,14 +1,24 @@
-import { FC, HTMLAttributes } from 'react'
+import { CSSProperties, FC, HTMLAttributes, ReactNode, useMemo } from 'react'
 import './style.css'
 
-interface TProps extends HTMLAttributes<HTMLDataElement> {}
+interface TProps extends HTMLAttributes<HTMLDataElement> {
+	minHeight?: CSSProperties['height']
+	loadingText?: string | ReactNode
+	visible: boolean
+}
 
 export const Skeleton: FC<TProps> = props => {
-	const { children } = props
+	const { children, minHeight, visible, loadingText } = props
+
+	const skeletonHeight = useMemo(() => {
+		return {
+			height: minHeight || 'fit-content',
+		}
+	}, [minHeight])
 	return (
-		<div className="skeleton-container">
+		<div className="skeleton-container" style={skeletonHeight}>
 			{children}
-			<div className="skeleton-mask"></div>
+			{visible && <div className="skeleton-mask">{loadingText}</div>}
 		</div>
 	)
 }
--- packages/snowweb/src/pages/snowDesign/index.tsx
+++ packages/snowweb/src/pages/snowDesign/index.tsx
@@ -1,9 +1,20 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
+import { Skeleton } from '../../components/snowDesign'
 
 export const SnowDesign: FC = () => {
+	const [skeletonVisible, setSkeletonVisible] = useState<boolean>(true)
+
+	useEffect(() => {
+		setTimeout(() => {
+			setSkeletonVisible(false)
+		}, 3000)
+	}, [])
+
 	return (
 		<div>
-			<h2>Skeleton</h2>
+			<Skeleton visible={skeletonVisible} loadingText={<div>加载中~~</div>}>
+				<h2>Skeleton</h2>
+			</Skeleton>
 		</div>
 	)
 }
