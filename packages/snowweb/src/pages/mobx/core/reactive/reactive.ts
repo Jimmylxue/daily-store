39c7548cbd59e41a2644592a37f37e14f6ef1dec
jimmyxuexue <1002661758@qq.com>
2023-04-26 16:28:29 +0800

    :zep: 支持深度响应式

--- packages/snowweb/src/pages/mobx/core/reactive/reactive.ts
+++ packages/snowweb/src/pages/mobx/core/reactive/reactive.ts
@@ -1,18 +1,20 @@
 type Dep = () => void
 
+type Target = { [key in string]: any }
+
 const map = new Map<string, Set<Dep>>()
 let temp: Dep
 
-export function reactive(obj: { [key in string]: any }) {
+export function reactive(obj: Target): any {
 	const proxy = new Proxy(obj, {
 		get(target, key, receiver) {
+			track(map, key)
+
 			const result = Reflect.get(target, key, receiver)
-			if (map.has(key as string)) {
-				map.get(key as string)?.add(temp)
-			} else {
-				map.set(key as string, new Set<Dep>().add(temp))
+			if (typeof result === 'object' && result !== null) {
+				return reactive(result)
 			}
-			return result
+			return Reflect.get(target, key, receiver)
 		},
 		set(target, key, value, receiver) {
 			Reflect.set(target, key, value, receiver)
@@ -20,8 +22,15 @@ export function reactive(obj: { [key in string]: any }) {
 			return true
 		},
 	})
-
 	return proxy
+}
+
+export function track(map: Map<string, Set<Dep>>, key: string | symbol) {
+	if (map.has(key as string)) {
+		map.get(key as string)?.add(temp)
+	} else {
+		map.set(key as string, new Set<Dep>().add(temp))
+	}
 }
 
 export function autoRun(fn: () => void) {
